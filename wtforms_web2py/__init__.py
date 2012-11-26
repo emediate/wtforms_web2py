2521712c07da74e1fe05171bd762fb09966291af
tn@0x746e.org.ua <tn@0x746e.org.ua>
2012-11-26 14:57:04 +0200

    Added monkeypatching to ensure web2py that fields are safe.

--- wtforms_web2py/__init__.py
+++ wtforms_web2py/__init__.py
@@ -1,3 +1,5 @@
-# TODO:
-# * db-driven select
-# * fields.py with fields that mark themselfs as save
+# HACK! Monkeypatching to ensure web2py that WTForms' widgets are safe.
+from wtforms.fields.core import Field
+from wtforms.widgets.core import HTMLString
+Field.xml = Field.__html__
+HTMLString.xml = HTMLString.__html__
