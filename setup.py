58d1e39a0fb4e519f43f23195dc342e0a98bfca2
tn@0x746e.org.ua <tn@0x746e.org.ua>
2013-04-24 18:35:23 +0300

    Bumped version to 0.1.7.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="wtforms_web2py",
-    version="0.1.6",
+    version="0.1.7",
     description="WTForms integration for web2py",
     packages=["wtforms_web2py"],
 )
